0e7d0c86bd359e66bbbd6bc26e54829662282fe3
Connor Henderson <connorhenderson20@gmail.com>
2022-10-27 14:16:10 -0400

    create meteor object file, import sqlite3 and create functions file

---
+++ functions.py
@@ -0,0 +1,26 @@
+def check_africa_middle_east_region():
+    print('hello')
+
+
+def check_europe():
+    print('hello')
+
+
+def check_upper_asia():
+    print('hello')
+
+
+def check_lower_asia():
+    print('hello')
+
+
+def check_australia():
+    print('hello')
+
+
+def check_north_america():
+    print('hello')
+
+
+def check_south_america():
+    print('hello')
---
+++ meteor_data_class.py
@@ -0,0 +1,75 @@
+class MeteorDataEntry:
+    def __init__(self):
+        pass
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, new_name):
+        self.name = new_name
+
+    def get_id(self):
+        return self.id
+
+    def set_id(self, new_id):
+        self.id = new_id
+
+    def get_nametype(self):
+        return self.nametype
+
+    def set_nametype(self, new_nametype):
+        self.nametype = new_nametype
+
+    def get_recclass(self):
+        return self.recclass
+
+    def set_recclass(self, new_recclass):
+        self.recclass = new_recclass
+
+    def get_mass(self):
+        return self.mass
+
+    def set_mass(self, new_mass):
+        self.mass = new_mass
+
+    def get_fall(self):
+        return self.fall
+
+    def set_fall(self, new_fall):
+        self.fall = new_fall
+
+    def get_year(self):
+        return self.year
+
+    def set_year(self, new_year):
+        self.year = new_year
+
+    def get_reclat(self):
+        return self.reclat
+
+    def set_reclat(self, new_reclat):
+        self.reclat = new_reclat
+
+    def get_reclong(self):
+        return self.reclong
+
+    def set_reclong(self, new_reclong):
+        self.reclong = new_reclong
+
+    def get_geoloc(self):
+        return self.geolocation
+
+    def set_geoloc(self, new_geoloc):
+        self.geloc = new_geoloc
+
+    def get_states(self):
+        return self.states
+
+    def set_states(self, new_state):
+        self.states = new_state
+
+    def get_counties(self):
+        return self.counties
+
+    def set_counties(self, new_county):
+        self.counties = new_county
