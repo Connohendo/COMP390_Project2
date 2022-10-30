43b7d784c78fc221dee1695e51b2197b3ff88d19
Connor Henderson <connorhenderson20@gmail.com>
2022-10-29 23:22:41 -0400

    finished product

--- functions.py
+++ functions.py
@@ -1,41 +1,76 @@
-
-# geolocation bounding box -- (left,bottom,right,top)
-bound_box_dict = {
-     'Africa_MiddleEast_Meteorites': (-17.8, -35.2, 62.2, 37.6),
-     'Europe_Meteorites': (-24.1, 36, 32, 71.1),
-     'Upper_Asia_Meteorites': (32.2, 35.8, 190.4, 72.7),
-     'Lower_Asia_Meteorites': (58.2, -9.9, 154, 38.6),
-     'Australia_Meteorites': (112.9, -43.8, 154.3, -11.1),
-     'North_America_Meteorites': (-168.2, 12.8, -52, 71.5),
-     'South_America_Meteorites': (-81.2, -55.8, -34.4, 12.6)
- }
+import json
+import sqlite3
+import main
 
 
-def check_africa_middle_east_region(geo_loc):
-    print(f'{geo_loc}')
+def check_geoloc(meteor):
+    if meteor.get('reclat') is None:
+        return False
+    if meteor.get('reclong') is None:
+        return False
+    return True
 
 
-def check_europe():
-    print('hello')
+def loop_through_jsondata(json_data, bound_box_dict, db_cursor):
+    for record in json_data:
+        if check_geoloc(record):
+            for key, value in bound_box_dict.items():
+                if value[1] <= convert_string_to_numerical(record.get('reclat')) <= value[3] and value[
+                    0] <= convert_string_to_numerical(record.get('reclong')) \
+                        <= value[2]:
+                    db_cursor.execute(f'INSERT INTO {key} VALUES(?, ?, ?, ?)',
+                                      (record.get('name', None),
+                                       record.get('mass', None),
+                                       record.get('reclat', None),
+                                       record.get('reclong', None)))
 
 
-def check_upper_asia():
-    print('hello')
+def create_tables(bound_box_dict, db_cursor):
+    for key in bound_box_dict:
+        create_table = f'CREATE TABLE IF NOT EXISTS {key}(name TEXT, mass TEXT, reclat TEXT, reclong TEXT);'
+        db_cursor.execute(create_table)
+        db_cursor.execute(f'DELETE FROM {key}')
+
+# The following 3 functions were created by Professor Matta at Bridgewater State University
 
 
-def check_lower_asia():
-    print('hello')
+def _string_is_int(in_string):
+    """ returns True if the incoming parameter is an int, returns False otherwise """
+    try:
+        int(in_string)
+        return True
+    except TypeError:
+        return False
+    except ValueError:
+        return False
 
 
-def check_australia():
-    print('hello')
+def _string_is_float(in_string):
+    """ returns True if the incoming parameter is a float, returns False otherwise """
+    try:
+        float(in_string)
+        return True
+    except TypeError:
+        return False
+    except ValueError:
+        return False
 
 
-def check_north_america():
-    print('hello')
+def convert_string_to_numerical(in_string):
+    """ this function converts a string to a numerical value (to either an int or float)
+        'None' is returned if the incoming string is not in the form of an int or float """
+    if _string_is_int(in_string):
+        return int(in_string)
+    elif _string_is_float(in_string):
+        return float(in_string)
+    return None
 
 
-def check_south_america():
-    print('hello')
-
-
+# this function takes two parameters: a dictionary object and a target key
+def convert_obj_to_string(dict_record, key):
+    # <dict>.get() gets the value associated with a key in a dictionary
+    # the second parameter for <dict>.get() tells the get function to return 'None' if the key does not exist
+    if dict_record.get(key, None) is None:
+        return None
+    # return a string version of the <dict>[key] object if a key is present
+    return json.dumps(dict_record[key])
--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import requests
 import json
 import sqlite3
 
-
-# this function takes two parameters: a dictionary object and a target key
-def convert_obj_to_string(dict_record, key):
-    # <dict>.get() gets the value associated with a key in a dictionary
-    # the second parameter for <dict>.get() tells the get function to return 'None' if the key does not exist
-    if dict_record.get(key, None) is None:
-        return None
-    # return a string version of the <dict>[key] object if a key is present
-    return json.dumps(dict_record[key])
+bound_box_dict = {
+    'Africa_MiddleEast_Meteorites': (-17.8, -35.2, 62.2, 37.6),
+    'Europe_Meteorites': (-24.1, 36, 32, 71.1),
+    'Upper_Asia_Meteorites': (32.2, 35.8, 190.4, 72.7),
+    'Lower_Asia_Meteorites': (58.2, -9.9, 154, 38.6),
+    'Australia_Meteorites': (112.9, -43.8, 154.3, -11.1),
+    'North_America_Meteorites': (-168.2, 12.8, -52, 71.5),
+    'South_America_Meteorites': (-81.2, -55.8, -34.4, 12.6)
+}
 
 
 def main():
@@ -30,72 +30,8 @@ def main():
         # create a cursor object - this cursor object will be used for all operations pertaining to the database
         db_cursor = db_connection.cursor()
 
-        # create a table in the database to store ALL the meteorite data (if it does not already exist)
-        # the parentheses following the table name ("meteorite_data") contains a list of column names
-        # and the data type of values that will be inserted into those columns
-        db_cursor.execute('''CREATE TABLE IF NOT EXISTS meteorite_data(
-                                name TEXT,
-                                id INTEGER,
-                                nametype TEXT,
-                                recclass TEXT,
-                                mass TEXT,
-                                fall TEXT,
-                                year TEXT,
-                                reclat TEXT,
-                                reclong TEXT,
-                                geolocation TEXT,
-                                states TEXT,
-                                counties TEXT);''')
-
-        # clear the 'meteorite_data' table if it already contains data from last time the program was run
-        db_cursor.execute('DELETE FROM meteorite_data')
-
-        # read all the data from the specified json URL and insert it into the database:
-        # loop through each dictionary entry in the JSON list
-        for record in json_data:
-            # some keys may not exist because there is no value for that record - use <dict>.get()
-            # to INSERT 'None' if the key is not found
-            db_cursor.execute('''INSERT INTO meteorite_data VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
-                              (record.get('name', None),
-                               int(record.get('id', None)),
-                               record.get('nametype', None),
-                               record.get('recclass', None),
-                               record.get('mass', None),
-                               record.get('fall', None),
-                               record.get('year', None),
-                               record.get('reclat', None),
-                               record.get('reclong', None),
-                               convert_obj_to_string(record, 'geolocation'),    # convert geolocation <dict> to string
-                               record.get(':@computed_region_cbhk_fwbd', None),
-                               record.get(':@computed_region_nnqa_25f4', None)))
-
-        # run a SELECT query on the table that holds all meteorite data
-        db_cursor.execute('SELECT * FROM meteorite_data WHERE id <= 1000')
-        # get the result of the query as a list of tuples
-        q1_result = db_cursor.fetchall()
-
-        # create a table in the database to store the filtered data (if it does not already exist)
-        # this table will hold the resulting rows of our SELECT query
-        db_cursor.execute('''CREATE TABLE IF NOT EXISTS filtered_data(
-                                               name TEXT,
-                                               id INTEGER,
-                                               nametype TEXT,
-                                               recclass TEXT,
-                                               mass TEXT,
-                                               fall TEXT,
-                                               year TEXT,
-                                               reclat TEXT,
-                                               reclong TEXT,
-                                               geolocation TEXT,
-                                               states TEXT,
-                                               counties TEXT);''')
-
-        # clear the 'filtered_data' table if it already contains data from last time the program was run
-        db_cursor.execute('DELETE FROM filtered_data')
-
-        # fill the filtered table
-        for tuple_entry in q1_result:
-            db_cursor.execute('''INSERT INTO filtered_data VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', tuple_entry)
+        functions.create_tables(bound_box_dict, db_cursor)
+        functions.loop_through_jsondata(json_data, bound_box_dict, db_cursor)
 
         # commit all changes made to the database
         db_connection.commit()
